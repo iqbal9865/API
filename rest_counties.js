1b2430ee96b569e4ccf07435f118d4bf02755165
Iqbal Ahmed <iqbalhussain9865@gmail.com>
2021-02-06 18:18:58 +0600

    modify

--- rest_counties.js
+++ rest_counties.js
@@ -7,15 +7,22 @@ const displayCountries = countries => {
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
         const countryDiv = document.createElement('div');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        const h4 = document.createElement('h4');
-        h4.innerText = country.region;
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(h4);
-        countryDiv.appendChild(p);
+        
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // const h4 = document.createElement('h4');
+        // h4.innerText = country.region;
+        // countryDiv.appendChild(h3);
+        // countryDiv.appendChild(h4);
+        // countryDiv.appendChild(p);
+
+        const countryInfo = `
+            <h3>${country.name}</h3>
+            <h4>${country.capital}</h4>
+        `;
+        countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     }
 }
